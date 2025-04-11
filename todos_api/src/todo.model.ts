06c467aea2401948d50666737cc01f3720cb1151
Sowparna Shome <soupornoshome@gmail.com>
2025-04-12 00:09:31 +0530

    model create

--- todos_api/src/todo.model.ts
+++ todos_api/src/todo.model.ts
@@ -1,8 +1,18 @@
-interface ITodo{
-    _id: string|Schema.Types.ObjectId;
+import mongoose, { Document, models, Schema } from "mongoose";
+
+interface ITodo extends Document{
     title: string;
     description?: string;
     completed: boolean|false;
     createdAt?: Date;
     updatedAt?: Date;
+    userId?: Schema.Types.ObjectId | string;
 }
+const todoSchema = new Schema<ITodo>({
+    userId: { type: Schema.Types.ObjectId, ref: 'User' },
+    title: { type: String },
+    description: { type: String},
+    completed: { type: Boolean, default: false },
+}, { timestamps: true });
+const Todo = models?.Todo||mongoose.model<ITodo>("Todo", todoSchema);
+export default Todo;
