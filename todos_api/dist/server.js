1e3118594ca2061e64f30bfd4d183e7cac7982fe
Sowparna Shome <soupornoshome@gmail.com>
2025-04-12 23:59:41 +0530

    comment emit and build then ts to js after build

---
+++ todos_api/dist/server.js
@@ -0,0 +1,4 @@
+import http from "http";
+import app from "./app.js";
+const server = http.createServer(app);
+server.listen(process.env.PORT, () => console.log(`Server is running at http://localhost:${process.env.PORT}`));
