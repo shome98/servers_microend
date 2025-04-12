03d69b48d8d36c66e91a36efc92b93b393a1453f
Sowparna Shome <soupornoshome@gmail.com>
2025-04-12 11:45:33 +0530

    check

--- todos_api/src/server.ts
+++ todos_api/src/server.ts
@@ -1,1 +1,5 @@
-console.log("jsut a server");
+import http from "http";
+import app from "./app";
+
+const server = http.createServer(app);
+server.listen(process.env.PORT, () => console.log(`Server is running at http://localhost:${process.env.PORT}`));
