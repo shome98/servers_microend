d2852617d2d28075587b80afa1bcca00d24fb5e0
Sowparna Shome <soupornoshome@gmail.com>
2025-04-12 23:18:17 +0530

    remove

--- todos_api/dist/server.js
+++
@@ -1,4 +0,0 @@
-import http from "http";
-import app from "./app";
-const server = http.createServer(app);
-server.listen(process.env.PORT, () => console.log(`Server is running at http://localhost:${process.env.PORT}`));
