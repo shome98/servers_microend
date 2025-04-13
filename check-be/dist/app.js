a31078c0552707a4ea8e55daa405ef9934f98185
Sowparna Shome <soupornoshome@gmail.com>
2025-04-13 15:00:31 +0530

    remove

--- check-be/dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.add = add;
-const teen_1 = require("./teen");
-function add(a, b) {
-    console.log((0, teen_1.teen)(67));
-    return a + b;
-}
--- check-be/dist/teen.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.teen = teen;
-function teen(a) {
-    return a;
-}
