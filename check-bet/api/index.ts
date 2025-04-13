c911f925c2d948200ce1c0b9f0a2a94fcd7efd6d
Sowparna Shome <soupornoshome@gmail.com>
2025-04-13 19:09:54 +0530

    app to index with export

--- check-bet/api/index.ts
+++ check-bet/api/index.ts
@@ -6,3 +6,4 @@ app.get("/", (req,res) => {
     res.send(`hello from vercel with ${add(89,87)}`);
 })
 app.listen(9000, () => console.log(`there is something running on http://localhost:${9000}`));
+export default app;
