23d1e095a797e3c1609ae76da132fb384e323ead
Sowparna Shome <soupornoshome@gmail.com>
2025-04-13 19:45:34 +0530

    file change

--- check-bet/api/index.ts
+++ check-bet/api/index.ts
@@ -1,9 +1,2 @@
-import express from "express";
-import { add } from "./add";
-const app = express();
-
-app.get("/", (req,res) => {
-    res.send(`hello from vercel with ${add(89,87)}`);
-})
-app.listen(9000, () => console.log(`there is something running on http://localhost:${9000}`));
+import app from "./app";
 export default app;
