ba43042216de89f6a13e77ca0c52434de94070d6
Sowparna Shome <soupornoshome@gmail.com>
2025-04-13 19:05:48 +0530

    check with vercel i guess

---
+++ check-bet/api/app.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { add } from "./add";
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send(`hello from vercel with ${add(89,87)}`);
+})
+app.listen(9000, () => console.log(`there is something running on http://localhost:${9000}`));
